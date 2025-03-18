e2743e9c367b1e3152d6e4ed99f03988a8162c1e
Luckeciano Carvalho Melo <lucelo@clpc297.cs.ox.ac.uk>
2025-03-18 19:03:35 +0000

    Adding train/evaluation scripts for oatcloud

---
+++ slurm/evaluate_oatcloud.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=96
+#SBATCH --gres=gpu:a100:4
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --exclusive
+#SBATCH --job-name="act-pm"
+#SBATCH --output=/users/lucelo/logs/slurm-%j.out
+#SBATCH --error=/users/lucelo/logs/slurm-%j.err
+
+export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
+export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs
+export XDG_CACHE_HOME=/scratch-ssd/oatml/
+
+export TMPDIR=/scratch/${USER}/tmp
+mkdir -p $TMPDIR
+BUILD_DIR=/scratch-ssd/${USER}/conda_envs/pip-build
+
+# Clean pip cache and set pip configurations
+rm -rf ~/.cache/pip
+export PIP_NO_CACHE_DIR=1
+export PIP_DEFAULT_TIMEOUT=100
+export PYTHONWARNINGS="ignore::DeprecationWarning"
+
+/scratch-ssd/oatml/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f ~/maxent-rl-r1/environment.yml
+source /scratch-ssd/oatml/miniconda3/bin/activate maxent-r1
+
+cd ~/maxent-rl-r1
+pip install --no-cache-dir --upgrade pip
+pip install --no-cache-dir -e ".[dev]"
+
+# Installing flash-attn
+pip install flash-attn --no-build-isolation
+
+echo $TMPDIR
+
+nvidia-smi
+
+huggingface-cli login --token $HUGGINGFACE_WRITETOKEN
+
+TASK_NAME=$1  # e.g. "eval_llm"
+TASKS="custom|aime24|0|0,custom|math_500|0|0,custom|gpqa:diamond|0|0"
+MODEL_ID=$2 # e.g. "Qwen/Qwen2.5-Math-7B"
+MODEL_REVISION=$3 # e.g. "b101308fe89651ea5ce025f25317fea6fc07e96e", the commit hash of the model
+MAX_MODEL_LENGTH=$4 # e.g. 32768
+# Optional args
+[ -z "$5"] && TENSOR_PARALLEL=False || TENSOR_PARALLEL=$5
+[ -z "$6"] && TRUST_REMOTE_CODE=False || TRUST_REMOTE_CODE=$6
+# $7 is reserved for system_prompt, see line 51
+NUM_GPUS=$(nvidia-smi -L | wc -l)
+
+# Set Whether to use tensor parallelism or data parallelism
+if [ "$TENSOR_PARALLEL" = "True" ]; then
+    # use TP to shard model across NUM_GPUS
+    export VLLM_WORKER_MULTIPROC_METHOD=spawn
+    # FIXME: lighteval now requires us to manually pass the generation params
+    MODEL_ARGS="pretrained=$MODEL_ID,revision=$MODEL_REVISION,trust_remote_code=$TRUST_REMOTE_CODE,dtype=bfloat16,tensor_parallel_size=$NUM_GPUS,max_model_length=$MAX_MODEL_LENGTH,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:$MAX_MODEL_LENGTH,temperature:0.6,top_p:0.95}"
+else
+    MODEL_ARGS="pretrained=$MODEL_ID,revision=$MODEL_REVISION,trust_remote_code=$TRUST_REMOTE_CODE,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=$MAX_MODEL_LENGTH,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:$MAX_MODEL_LENGTH,temperature:0.6,top_p:0.95}"
+fi
+
+LM_EVAL_REPO_ID="luckeciano/maxent-rl-eval-leaderboard"
+MODEL_NAME=$(echo $MODEL_ID | sed 's/\//_/g') # replaces / with _
+DETAILS_REPO_ID="luckeciano/maxent-rl-details-$MODEL_NAME"
+OUTPUT_DIR="eval_results/$MODEL_ID/$MODEL_REVISION/$TASK_NAME"
+# We need this flag since we run this script from training jobs that use DeepSpeed and the env vars get progated which causes errors during evaluation
+ACCELERATE_USE_DEEPSPEED=false
+# Enable fast downloads
+HF_HUB_ENABLE_HF_TRANSFER=1
+
+echo "Running lighteval script ..."
+echo "Eval results will be saved to $OUTPUT_DIR"
+# Check if "custom" is a substring of TASKS
+if [[ $TASKS == *"custom"* ]]; then
+    echo "Custom task detected. Running custom task evaluation script ..."
+    lighteval vllm "$MODEL_ARGS" $TASKS \
+    --custom-tasks "src/open_r1/evaluate.py" \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR \
+    --save-details \
+    ${7:+--system-prompt "$7"}
+else
+    lighteval vllm "$MODEL_ARGS" $TASKS \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR \
+    --save-details \
+    ${7:+--system-prompt "$7"}
+fi
+
+OUTPUT_FILEPATHS=$(find $OUTPUT_DIR/results/ -type f \( -name "*.json" \))
+for filepath in $OUTPUT_FILEPATHS; do
+    echo "Uploading $filepath to Hugging Face Hub..."
+    filename=$(basename -- "$filepath")
+    for attempt in {1..20}; do
+        if huggingface-cli upload --repo-type space --private $LM_EVAL_REPO_ID $filepath $OUTPUT_DIR/$filename; then
+            echo "Upload succeeded for $filepath"
+            break
+        else
+            echo "Upload failed for $filepath. Attempt $attempt of 20. Retrying in 5 seconds..."
+            sleep 5
+        fi
+    done
+done
+
+echo "Uploading details to Hugging Face Hub..."
+DETAILS_FILEPATHS=$(find $OUTPUT_DIR/details/ -type f \( -name "*.parquet" \))
+echo "DETAILS_FILEPATHS: $DETAILS_FILEPATHS"
+TIMESTAMP=$(date +"%Y-%m-%dT%H-%M-%S")
+python scripts/upload_details.py --data_files $DETAILS_FILEPATHS --hub_repo_id $DETAILS_REPO_ID --config_name $MODEL_REVISION.$TASK_NAME.$TIMESTAMP
+    
+echo "Cleaning up ..."
+rm -rf $OUTPUT_DIR
+
+echo "Done!"
---
+++ slurm/train_oatcloud_v2.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=96
+#SBATCH --gres=gpu:a100:4
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --exclusive
+#SBATCH --job-name="act-pm"
+#SBATCH --output=/users/lucelo/logs/slurm-%j.out
+#SBATCH --error=/users/lucelo/logs/slurm-%j.err
+
+export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
+export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs
+export XDG_CACHE_HOME=/scratch-ssd/oatml/
+
+export TMPDIR=/scratch/${USER}/tmp
+mkdir -p $TMPDIR
+BUILD_DIR=/scratch-ssd/${USER}/conda_envs/pip-build
+
+# Clean pip cache and set pip configurations
+rm -rf ~/.cache/pip
+export PIP_NO_CACHE_DIR=1
+export PIP_DEFAULT_TIMEOUT=100
+export PYTHONWARNINGS="ignore::DeprecationWarning"
+
+/scratch-ssd/oatml/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f ~/maxent-rl-r1/environment.yml
+source /scratch-ssd/oatml/miniconda3/bin/activate maxent-r1
+
+cd ~/maxent-rl-r1
+pip install --no-cache-dir --upgrade pip
+pip install --no-cache-dir -e ".[dev]"
+
+# Installing flash-attn
+pip install flash-attn --no-build-isolation
+
+echo $TMPDIR
+
+nvidia-smi
+
+huggingface-cli login --token $HUGGINGFACE_WRITETOKEN
+
+echo "START TIME: $(date)"
+
+MODEL=Qwen2.5-Math-7B
+TASK=grpo
+CONFIG_SUFFIX=oatcloud
+ACCELERATOR=zero3_oatcloud
+OPTIONAL_ARGS=
+
+# Training setup
+NUM_NODES=$SLURM_NNODES
+GPUS_PER_NODE=4
+WORLD_SIZE=$(($NUM_NODES*$GPUS_PER_NODE))
+# Due to conflicts between Accelerate's DeepSpeed configs and Transformers' TrainingArguments, we need to parse the gradient accumulation steps from the config file to ensure they match
+CONFIG_FILE=recipes/$MODEL/$TASK/config_$CONFIG_SUFFIX.yaml
+GRAD_ACC_STEPS=$(grep 'gradient_accumulation_steps' $CONFIG_FILE | awk '{print $2}')
+
+# Check if we are running vLLM during training to adjust the world size
+if grep -q 'use_vllm:\s*true' "$CONFIG_FILE"; then
+    USE_VLLM="true"
+else
+    USE_VLLM="false"
+fi
+
+if [[ "$USE_VLLM" == "true" ]]; then
+    WORLD_SIZE=$(($WORLD_SIZE - 1))
+fi
+
+# Split the string into individual arguments
+IFS=' ' read -ra ARGS <<< "$OPTIONAL_ARGS"
+
+# Loop through the arguments and find the one with "--gradient_accumulation_steps"
+for arg in "${ARGS[@]}"; do
+    if [[ "$arg" == "--gradient_accumulation_steps="* ]]; then
+        # Extract the value after the equals sign
+        GRAD_ACC_STEPS="${arg#*=}"
+        break  # Exit the loop once we find the desired argument
+    fi
+done
+
+echo "Gradient accumulation steps: $GRAD_ACC_STEPS"
+# so processes know who to talk to
+MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
+MASTER_PORT=29500
+
+echo "MASTER_ADDR: $MASTER_ADDR"
+echo "MASTER_PORT: $MASTER_PORT"
+echo "NUM_NODES: $NUM_NODES"
+echo "WORLD_SIZE: $WORLD_SIZE"
+echo "GRAD_ACC_STEPS: $GRAD_ACC_STEPS"
+echo "CONFIG_FILE: $CONFIG_FILE"
+echo "OPTIONAL_ARGS: $OPTIONAL_ARGS"
+echo "ACCELERATOR: $ACCELERATOR"
+echo "TASK: $TASK"
+echo "SLURM_PROCID: $SLURM_PROCID"
+
+# Add these environment variables before the accelerate launch command
+export CUDA_VISIBLE_DEVICES=$(nvidia-smi --query-gpu=index --format=csv,noheader | tr '\n' ',' | sed 's/,$//')
+
+export CMD=" \
+    src/open_r1/$TASK.py --config $CONFIG_FILE $OPTIONAL_ARGS
+    "
+
+export LAUNCHER="HF_HUB_ENABLE_HF_TRANSFER=1 ACCELERATE_LOG_LEVEL=info TRANSFORMERS_VERBOSITY=info accelerate launch \
+    --config_file recipes/accelerate_configs/$ACCELERATOR.yaml  \
+    --gradient_accumulation_steps $GRAD_ACC_STEPS \
+    --num_machines $NUM_NODES \
+    --num_processes $WORLD_SIZE \
+    --main_process_ip $MASTER_ADDR \
+    --main_process_port $MASTER_PORT \
+    --machine_rank $SLURM_PROCID \
+    --rdzv_conf "rdzv_backend=c10d,rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT" \
+    --max_restarts 1 \
+    --role \$(hostname -s): \
+    --tee 3 \
+    "
+
+# force crashing on nccl issues like hanging broadcast
+export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
+export NCCL_DEBUG=WARN
+export NCCL_ASYNC_ERROR_HANDLING=1
+export NCCL_BLOCKING_WAIT=1
+export NCCL_P2P_DISABLE=1
+export CUDA_LAUNCH_BLOCKING=1
+# export NCCL_DEBUG=INFO
+# export NCCL_DEBUG_SUBSYS=COLL
+# export NCCL_SOCKET_NTHREADS=1
+# export NCCL_NSOCKS_PERTHREAD=1
+# export CUDA_LAUNCH_BLOCKING=1
+
+# srun error handling:
+# --wait=60: wait 60 sec after the first task terminates before terminating all remaining tasks
+# --kill-on-bad-exit=1: terminate a step if any task exits with a non-zero exit code
+SRUN_ARGS=" \
+    --wait=60 \
+    --kill-on-bad-exit=1 \
+    "
+
+clear; srun $SRUN_ARGS --jobid $SLURM_JOB_ID bash -c "$LAUNCHER --role \$SLURMD_NODENAME: $CMD" 2>&1
+
+echo "END TIME: $(date)"
